3d07796ffc89a5c67882b63617e4d88452e5a3bd
ilkinallahverdiyev <135498071+ilkinallahverdiyev@users.noreply.github.com>
2023-06-16 11:45:53 +0400

    Create Task2(product)class.cs

---
+++ Task2(product)class.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ilkin
+{
+    public class Product
+    {
+        public string Name;
+        public int  price;
+       
+      
+   
+
+
+    }
+}
